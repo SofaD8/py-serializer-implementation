a83e106539cba4fdc07e624239fb04be39eae750
Sofa <sofo4kavakula@gmail.com>
2025-12-29 12:23:39 +0200

    Create serializer implementation

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
+from car.models import Car
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(1914)]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = (
+        serializers.CharField(allow_null=True, required=False)
+    )
+
+    def create(self, validated_data):
+        return Car(**validated_data)
+
+    def to_representation(self, instance):
+        return {
+            "manufacturer": instance.manufacturer,
+            "model": instance.model,
+            "horse_powers": instance.horse_powers,
+            "is_broken": instance.is_broken,
+            "problem_description": instance.problem_description,
+        }
