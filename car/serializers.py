741047f7335246a71cfd5d7e3bb939023c6a998e
Sofa <sofo4kavakula@gmail.com>
2025-12-29 12:45:20 +0200

    Fixed serializers

--- car/serializers.py
+++ car/serializers.py
@@ -1,24 +1,26 @@
 from rest_framework import serializers
 from car.models import Car
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(1914)]
     )
     is_broken = serializers.BooleanField()
-    problem_description = (
-        serializers.CharField(allow_null=True, required=False)
+    problem_description = serializers.CharField(
+        allow_null=True, required=False, allow_blank=True
     )
 
     def create(self, validated_data):
-        return Car(**validated_data)
+        return Car.objects.create(**validated_data)
 
     def to_representation(self, instance):
         return {
+            "id": instance.id,
             "manufacturer": instance.manufacturer,
             "model": instance.model,
             "horse_powers": instance.horse_powers,
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(serializer.data).encode("utf-8")
+    return json.dumps(
+        serializer.data,
+        separators=(",", ":")
+    ).encode("utf-8")
 
 def deserialize_car_object(data: bytes) -> Car:
     parsed = json.loads(data.decode("utf-8"))
     serializer = CarSerializer(data=parsed)
     serializer.is_valid(raise_exception=True)
-    return serializer.create(serializer.validated_data)
+    return serializer.save()
